516f6a57fb373e716277561bd8bb963d05ac13a1
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-11-12 08:14:16 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px', fontSize: '20px', fontWeight: 'bold', color: 'blue', textShadow: '1px 1px 1px black', fontFamily: 'Arial' }}>
+        Hello World
+    </div>
+  );
+}
+
+export default App;
